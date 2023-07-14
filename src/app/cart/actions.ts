cc26b05e8763f04cda4a6f983d48d06b8311a113
WYZ00 <thanthtettin@gmail.com>
2023-07-15 03:44:09 +0630

    change prisma extensions for db

--- src/app/cart/actions.ts
+++ src/app/cart/actions.ts
@@ -11,24 +11,44 @@ export async function setProductQuantity(productId: string, quantity: number) {
 
   if (quantity === 0) {
     if (articleInCart) {
-      await prisma.cartItem.delete({
+      await prisma.cart.update({
         where: {
-          id: articleInCart.id,
+          id: cart.id,
+        },
+        data: {
+          items: {
+            delete: { id: articleInCart.id },
+          },
         },
       });
     }
   } else {
     if (articleInCart) {
-      await prisma.cartItem.update({
-        where: { id: articleInCart.id },
-        data: { quantity },
+      await prisma.cart.update({
+        where: {
+          id: cart.id,
+        },
+        data: {
+          items: {
+            update: {
+              where: { id: articleInCart.id },
+              data: { quantity },
+            },
+          },
+        },
       });
     } else {
-      await prisma.cartItem.create({
+      await prisma.cart.update({
+        where: {
+          id: cart.id,
+        },
         data: {
-          cartId: cart.id,
-          productId,
-          quantity,
+          items: {
+            create: {
+              productId,
+              quantity,
+            },
+          },
         },
       });
     }
